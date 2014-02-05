4528dee7c3fccecc3d02a2af9b5645af0d4747fc
MesiterWebzr <meister@fullsail.edu>
2014-02-05 10:56:58 -0800

    formula
    works correctly

--- wk1/goal1_assign_duel1/js/main.js
+++ wk1/goal1_assign_duel1/js/main.js
@@ -40,6 +40,11 @@
 		//random number formula is Math.floor(Math.random() * (max-min) + min)
 		var minDamage1 = player1Damage * .5;
 		var minDamage2 = player2Damage * .5;
+		var f1 = Math.floor(Math.random() * (player1Damage-minDamage1) + minDamage1);
+		var f2 = Math.floor(Math.random() * (player2Damage-minDamage2) + minDamage2);
+
+		//console.log(f1);
+		//console.log(f2);
 
 
 		};
