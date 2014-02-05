6d1e4b3cffd465d5813af28572ea35d521e9669f
MesiterWebzr <meister@fullsail.edu>
2014-02-05 10:04:35 -0800

    Directory
    setup all files folders and layout for main js files

---
+++ wk1/goal1_assign_duel1/js/main.js
@@ -0,0 +1,36 @@
+/*
+	Author:Otto "Meister" Burroughs
+	Date: 02/05/14
+	Assignment: Goal1 - Duel1
+*/
+
+
+//Self executing function
+(function(){
+
+	console.log("FIGHT!!!");
+
+	//players name
+	var playerOneName = Meister
+	var playerTwoName = Webzr
+
+
+	//players damage
+	var player1Damage = 20;
+	var player2Damage = 20;
+
+	//player health
+	var playerOneHealth = 100;
+	var playerTwoHealth = 100;
+
+	//rounds
+	var round = 1;
+
+	function fight(){};
+
+	function winnerCheck(){};
+
+	fight(); //this is where the program begins
+
+})();
+
